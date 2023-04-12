b16ab71fd8440d6907ecf3fd1b80954aca840c24
nikalitvinchuk <116036914+nikalitvinchuk@users.noreply.github.com>
2023-04-12 18:12:21 +0200

    First web

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './App.css';
+import MainPage from './MainPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+class App extends Component { 
+  render(){
+  return (
+    <MainPage/>
+  )
+}
+}
+export default App;
