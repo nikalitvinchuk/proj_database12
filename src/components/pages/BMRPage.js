8b1d3eee9a9c77e6419ee3a07987ae067e3ba7bb
nikalitvinchuk <116036914+nikalitvinchuk@users.noreply.github.com>
2023-04-20 00:05:07 +0200

    Kalkulator wody

--- src/components/pages/BMRPage.js
+++ src/components/pages/BMRPage.js
@@ -1,20 +1,55 @@
-import React from 'react';
-const BMRPage = () => {
-    return ( 
-        <section id="hero" className="d-flex.align-items-center justify-content-center">
-            <div className="container text-center">
-                <div className="row justify-content-center">
-                    <div className="col-md-6 col-lg-8 mb-3">
-                        <h1 className="nagl1"> BMR</h1>
-                        <h2>Chcemy pomóc Ci zadbać o swoje zdrowie i dobre samopoczucie. W naszej poradni oferujemy szeroki zakres usług, które pomogą Ci w osiągnięciu Twoich celów.</h2>
-                        <h3>Nieobecny traci</h3>
-                    </div>
-                </div>
-  
+import React from "react";
+import "../styles/BMI.css";
 
-            </div>
-        </section>
-    );
-}
- 
-export default BMRPage;
+export default function BMRPage () {
+
+  return (
+    <div class="bmi_main">
+      <div class="container_bmi">
+        <h1>BASAL METABOLIC RATE (BMR) Kalkulator</h1>
+        <div class="form-item">
+          <label>Płeć:</label>
+          <select>
+            <option value="">Wybierz płeć</option>
+            <option value="male">Mężczyzna</option>
+            <option value="female">Kobieta</option>
+          </select>
+        </div>
+        <div class="form-item">
+          <label>Wzrost [cm]:</label>
+          <input type="number" />
+        </div>
+        <div class="form-item">
+          <label>Waga [kg]:</label>
+          <input type="number" />
+        </div>
+        <div class="form-item">
+          <label>Wiek:</label>
+          <input type="number" />
+        </div>
+        <div class="form-item">
+          <label>Aktywność fizyczna:</label>
+          <select >
+            <option value="">Wybierz aktywność fizyczną</option>
+            <option value="1.2">Brak aktywności fizycznej / praca siedząca</option>
+            <option value="1.4">Niska aktywność fizyczna / praca siedząca + 1-2 treningi w tygodniu</option>
+            <option value="1.6">Średnia aktywność fizyczna / praca siedząca + 3-4 treningi w tygodniu</option>
+            <option value="1.8">Wysoka aktywność fizyczna / praca fizyczna + 3-4 treningi w tygodniu</option>
+            <option value="2">Bardzo wysoka aktywność fizyczna / praca fizyczna + codzienne treningi</option>
+          </select>
+        </div>
+        <button class="btn licz">
+          OBLICZ
+        </button>
+        <button class="btn reset">
+          RESET
+        </button>
+        { (
+          <div class="result">
+            Twoje BMR wynosi:  kcal. Aby utrzymać wagę, powinieneś spożywaćm  około  kcal.
+          </div>
+        )}
+      </div>
+    </div>
+  );
+        }  
--- src/components/pages/WaterPage.js
+++ src/components/pages/WaterPage.js
@@ -1,25 +1,50 @@
-import React from "react";
-const WaterPage = () => {
-  return (
-    <section
-      id="hero"
-      className="d-flex.align-items-center justify-content-center"
-    >
-      <div className="container text-center">
-        <div className="row justify-content-center">
-          <div className="col-md-6 col-lg-8 mb-3">
-            <h1 className="nagl1"> WATER</h1>
-            <h2>
-              Chcemy pomóc Ci zadbać o swoje zdrowie i dobre samopoczucie. W
-              naszej poradni oferujemy szeroki zakres usług, które pomogą Ci w
-              osiągnięciu Twoich celów.
-            </h2>
-            <h3>Nieobecny traci</h3>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
+import React, { useState } from "react";
+import "../styles/BMI.css";
 
-export default WaterPage;
+export default function WaterIntakePage() {
+
+  const [weight, setWeight] = useState("");
+  const [waterIntake, setWaterIntake] = useState("");
+
+  const calculateWaterIntake = () => {
+    if (weight <= 0) {
+      alert("Podaj poprawną wagę");
+    } else {
+      const waterIntakeValue = (weight * 0.04).toFixed(2);
+      setWaterIntake(waterIntakeValue);
+    }
+  };
+
+  const reset = () => {
+    setWeight("");
+    setWaterIntake("");
+  };
+
+  return (
+    <div className="bmi_main">
+      <div className="container_bmi">
+        <h1>Kalkulator ilości wody do picia</h1>
+
+        <div className="form-item">
+          <label>Waga [kg]</label>
+          <input
+            type="number"
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+          />
+        </div>
+        <button className="btn licz" onClick={calculateWaterIntake}>
+          OBLICZ
+        </button>
+        <button className="btn reset" onClick={reset}>
+          RESET
+        </button>
+        {waterIntake && (
+          <div className="result">
+            Twoje zalecane spożycie wody na dzień wynosi: {waterIntake} litrów.
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
