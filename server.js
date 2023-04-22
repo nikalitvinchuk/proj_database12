689e51534c2d949c66051f282643ca88648a509d
MORDI4 <mmordarski4@gmail.com>
2023-04-22 18:31:03 +0200

    Połączenie z bazą danych

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql');
 const app = express();
+
+// Konfiguracja po³¹czenia z baz¹ danych
+const db = mysql.createConnection({
+    user: "admin",
+    host: "database.cygpzv8ws2xs.eu-central-1.rds.amazonaws.com",
+    password: "12345678",
+    database: "PZTZ",
+    port: 3307
+});
+
+// Nawi¹zanie po³¹czenia z baz¹ danych
+db.connect((error) => {
+    if (error) {
+        console.error('B³¹d po³¹czenia z baz¹ danych:', error);
+    } else {
+        console.log('Po³¹czono z baz¹ danych.');
+    }
+});
 
 // ustawienie katalogu, w którym znajduj¹ siê pliki statyczne
 app.use(express.static(path.join(__dirname, 'build')));
