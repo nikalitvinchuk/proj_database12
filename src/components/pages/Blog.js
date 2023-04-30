d8bf4cd6016ea0303dc9763de0878675b86879d3
nikalitvinchuk <116036914+nikalitvinchuk@users.noreply.github.com>
2023-04-30 19:13:03 +0200

    Blog - view

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -1,21 +1,25 @@
 import React from "react";
 const Blog = () => {
-  return (
-    <section
-      id="hero"
-      className="d-flex.align-items-center justify-content-center"
-    >
-      <div className="container text-center">
-        <div className="row justify-content-center">
-          <div className="col-md-6 col-lg-8 mb-3">
-            <h1 className="nagl1"> Blog</h1>
-          </div>
-        </div>
-
-        <div className="row mt-5 justify-content-center"></div>
-      </div>
-    </section>
-  );
+    return (
+        <section
+            id="hero"
+            className="d-flex.align-items-center justify-content-center"
+        >
+            <div className="container text-center">
+                <div className="row justify-content-center">
+                    <div className="col-md-6 col-lg-8 mb-3">
+                        <h1 className="nagl1"> Blog - wybierz temat </h1> <br />
+                        <div className="topic_blog">
+                            <button>Zdrowie</button>
+                            <button>Zabiegi</button>
+                            <button>Æwiczenia</button>
+                            <button>Lokalizacje</button>
+                        </div>
+                        <div className="blog_result"></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
-
 export default Blog;
