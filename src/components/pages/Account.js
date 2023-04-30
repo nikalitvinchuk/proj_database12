9222f3fb1fb507d3e000c40f6ac03a3e8f41b10a
nikalitvinchuk <116036914+nikalitvinchuk@users.noreply.github.com>
2023-04-30 15:33:00 +0200

    Page - My account

--- src/components/pages/Account.js
+++ src/components/pages/Account.js
@@ -1,36 +1,36 @@
-import React from 'react';
+import React from "react";
 const Account = () => {
-    return ( 
-      <section className="main_form">
-      <div className="form-box">
-          <div className="form-value">
-              <form id="login-form">
-                  <h2>Twoje Konto</h2>
-                  <div className="inputbox_acc">
-                      <label>Imię: </label>
-                  </div>
-                  <div className="inputbox_acc">
-                      <label>Nazwisko: </label>
-                  </div>
-                  <div className="inputbox_acc">
-                      <label>Email:</label>
-                  </div>   
-                  <div className="inputbox_acc">
-                      <label>Hasło:</label>
-                  </div>   
-                  <div className="inputbox_acc">
-                      <label>Zainteresowania:</label>
-                  </div>
-                  <div className="inputbox_acc">
-                      <label>Typ konta:</label>
-                  </div>   
-                  <button type="button">Zmień</button>.
-                  
-              </form>
-          </div>
-      </div>
-  </section>
+    return (
+        <section className="main_form">
+            <div className="form-box">
+                <div className="form-value">
+                    <form id="login-form">
+                        <h2>Twoje Konto</h2>
+                        <div className="inputbox_acc">
+                            <label>Imię</label>
+                            <input type="text" placeholder="Imię" /> <br />
+                            <label>Nazwisko</label>
+                            <input type="text" placeholder="Nazwisko" /> <br />
+                            <label>Email</label>
+                            <input type="text" placeholder="Email" /> <br />
+                            <label>Hasło</label>
+                            <input type="password" placeholder="Hasło" /> <br />
+                            <label>Typ konta</label>
+                            <input type="text" placeholder="User" /> <br />
+                            <label>Zainteresowania</label>
+                            <select>
+                                <option>Sport</option>
+                                <option>Zdrowie</option>
+                                <option>Zdrowe odżywianie</option>
+                                <option>Rady na codzień</option>
+                            </select> <br />
+                        </div>
+                        <button type="button">Zmień</button>.
+                    </form>
+                </div>
+            </div>
+        </section>
     );
-}
- 
+};
+
 export default Account;
