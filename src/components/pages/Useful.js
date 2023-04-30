5da0989c4b8785029547929175fa7eb06f976d88
nikalitvinchuk <116036914+nikalitvinchuk@users.noreply.github.com>
2023-04-30 19:47:19 +0200

    Przydatne - view

--- src/components/pages/Useful.js
+++ src/components/pages/Useful.js
@@ -1,21 +1,22 @@
 import React from "react";
 const Useful = () => {
-  return (
-    <section
-      id="hero"
-      className="d-flex.align-items-center justify-content-center"
-    >
-      <div className="container text-center">
-        <div className="row justify-content-center">
-          <div className="col-md-6 col-lg-8 mb-3">
-            <h1 className="nagl1"> Przydatne</h1>
-          </div>
-        </div>
-
-        <div className="row mt-5 justify-content-center"></div>
-      </div>
-    </section>
-  );
+    return (
+        <section
+            id="hero"
+            className="d-flex.align-items-center justify-content-center"
+        >
+            <div className="container text-center">
+                <div className="row justify-content-center">
+                    <div className="col-md-6 col-lg-8 mb-3">
+                        <h1 className="nagl1"> Wylosuj wskazówkê na dziœ</h1>
+                        <button className="button_tips">Losuj</button>
+                        <div className="result_useful">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Useful;
