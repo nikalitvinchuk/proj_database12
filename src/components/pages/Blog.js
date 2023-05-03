f93b3ab263eb497b79914c199964aeb984d3a6f9
nikalitvinchuk <116036914+nikalitvinchuk@users.noreply.github.com>
2023-05-04 00:46:09 +0200

    Blog - rozbudowa - wyswietlanie komponentow

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -1,5 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 const Blog = () => {
+
+    const [category, setCategory] = useState("Zdrowie");
+    const posts = [
+        {
+            title: "Post 1",
+            category: "Zdrowie",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        },
+        {
+            title: "Post 2",
+            category: "Zabiegi",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        },
+        {
+            title: "Post 3",
+            category: "Æwiczenia",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        },
+        {
+            title: "Post 4",
+            category: "Lokalizacje",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        },
+        {
+            title: "Post 5",
+            category: "Zdrowie",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        },
+        {
+            title: "Post 6",
+            category: "Æwiczenia",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        },
+        {
+            title: "Post 7",
+            category: "Zdrowie",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        },
+    ];
+
+    const handleClick = (cat) => {
+        setCategory(cat);
+    };
+
+    const filteredPosts = posts.filter((post) => post.category === category);
+
+
     return (
         <section
             id="hero"
@@ -10,16 +57,26 @@ const Blog = () => {
                     <div className="col-md-6 col-lg-8 mb-3">
                         <h1 className="nagl1"> Blog - wybierz temat </h1> <br />
                         <div className="topic_blog">
-                            <button>Zdrowie</button>
-                            <button>Zabiegi</button>
-                            <button>Æwiczenia</button>
-                            <button>Lokalizacje</button>
+                            <button onClick={() => handleClick("Zdrowie")}>Zdrowie</button>
+                            <button onClick={() => handleClick("Zabiegi")}>Zabiegi</button>
+                            <button onClick={() => handleClick("Æwiczenia")}>Æwiczenia</button>
+                            <button onClick={() => handleClick("Lokalizacje")}>Lokalizacje</button>
+                            <br /><br />
                         </div>
-                        <div className="blog_result"></div>
+                        <div className="blog_result">
+                            {filteredPosts.map((post, index) => (
+                                <div key={index} className="post">
+                                    <h4 className="post_title">{post.title}</h4>
+                                    <p className="post_content">{post.content}</p>
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
+
             </div>
         </section>
     );
 };
+
 export default Blog;
